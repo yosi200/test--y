3eecc16333906110539e89a8eb4525b4eb63104c
[moti] <[yosipur@gmail.com]>
2024-09-07 17:02:21 +0300

    792024

---
+++ JAVASCRIPT/basic.js
@@ -0,0 +1,82 @@
+//2.Primitive Types//
+
+// const str = "Yosi Pur";/*E1*/ 
+// const num1=1, num2=2;/*E2*/ 
+// console.log(num1+num2);
+// const boolean=true;/*E3*/ 
+// let without;/*E4*/ 
+// console.log(without);
+// without=0; /*E5*/ 
+// console.log(without);
+
+// 3.Variable Naming//
+
+// const parisTemperature = 20;/*E6*/
+// const isLoggedIn = false;/*E7*/ 
+// const customerEmailFromForm = "Yosipur@gmail.com";/*E8*/
+// const totalBasketballScore = '102-81';/*E9*/
+// console.log(totalBasketballScore); /*E10*/
+
+//4.Understading Let ,const,var//
+
+// var myNumber = 42;/*E11*/
+// let myString = "Hello, world!";/*E12*/
+// const isTrue = true;/*E13*/
+
+// var myVariable = 10;/*E14*/ 
+// console.log(myVariable); 
+// myVariable = 20;
+// console.log(myVariable); 
+
+// let myVariable = 10;/*E15*/ 
+// console.log(myVariable); 
+
+// myVariable = 20;
+// console.log(myVariable);
+
+// const myConstant = 10; /*E16*/
+// console.log(myConstant); 
+// myConstant = 20;
+// console.log(myConstant); 
+
+//5.Template Literals//
+
+// const myName = "Yosi";/*E17*/ 
+// const myAge = 34;
+// const greeting = `My name is ${myName} and I am ${myAge} years old.`;
+// console.log(greeting);
+
+// const length = 5;/*E18*/
+// const width = 3;
+// const area = length * width;
+// const result = `The area of a rectangle with length of ${length} and width of ${width} is ${area}`;
+// console.log(result);
+
+// const length = 5; /*E19*/
+// const width = 3; 
+// const area = length * width;
+// console.log(`The area of a rectangle with length of ${length} and width of ${width} is ${area}`);
+
+// const today=new Date();/*E20*/
+// const task1="complete my homework";
+// const task2="go to a run";
+// const task3="read a book";
+// const schedule = `On ${today},I will ${task1}, and ${task2}, and ${task3}.`;
+// console.log(schedule);
+
+// const numberOfPets = 2; /*E21*/ 
+
+// const sentence = `I have ${numberOfPets} pets.`;
+
+// console.log(sentence);
+
+// const base = 10; // הגדרת הבסיס
+// const height = 5; // הגדרת הגובה 
+
+// const area = (base * height) / 2;//הצהרה כולל השמה , סוגריים עבור חישוב מיוחד 
+
+// console.log(`The area of a triangle with base ${base} and height ${height} is ${area}`);
+
+//6.typeof operator//
+//7.String concatenation//
+
